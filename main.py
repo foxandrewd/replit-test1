c33da650002d981f2bb2b4264c58fcef7c27928f
foxandrewd <fox.andrew.d@gmail.com>
2020-09-07 13:01:26 +0000

    Added the req'd brackets to the call to os.getcwd!!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
-cwd = os.getcwd
 
-print("The Current Working Directory (CWD) is: " + cwd)
+cwd = os.getcwd()
+
+print("The Current Working Directory (CWD) is:\n"+cwd+'\n')
